23037529fc768756cca0d8198ac26a14d1c89935
nithy <nithy@NITBA>
2023-04-29 07:25:51 -0400

    added ExtentTest Logger

--- src/test/java/com/qa/actitime/testcases/BaseTest.java
+++ src/test/java/com/qa/actitime/testcases/BaseTest.java
@@ -4,6 +4,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.qa.actitime.driver.DriverManager;
 import com.qa.actitime.pages.Loginpage;
@@ -11,8 +12,9 @@ import com.qa.actitime.pages.Loginpage;
 public class BaseTest extends DriverManager{
 	
 	Loginpage lp;
-	ExtentHtmlReporter extent;
-	ExtentReports report;
+	protected static ExtentHtmlReporter extent;
+	protected static ExtentReports report;
+	protected static ExtentTest Logger;
 	
 public void setUpReport() {
 	
